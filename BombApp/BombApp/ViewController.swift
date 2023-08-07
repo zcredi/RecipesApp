213b7e1f832a429d68cf68b93257af4aaeeaba6f
Vladislav <107634092+zcredi@users.noreply.github.com>
2023-08-07 10:00:02 +0300

    add main

---
+++ BombApp/BombApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BombApp
+//
+//  Created by Владислав on 07.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Udemy Anjela/BombApp/BombApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BombApp
+//
+//  Created by Владислав on 07.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
