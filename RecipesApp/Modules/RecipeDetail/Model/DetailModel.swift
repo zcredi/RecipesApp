0eb2cbbf834f7bc23f03bf9e222d65db520c425c
Andrew Frolenkov <andrewissuess@yahoo.com>
2023-09-01 13:57:45 +0300

    Create DetailRecipeModel and bugfix

---
+++ RecipesApp/Modules/RecipeDetail/Model/DetailModel.swift
@@ -0,0 +1,17 @@
+//
+//  DetailModel.swift
+//  RecipesApp
+//
+//  Created by Андрей Фроленков on 1.09.23.
+//
+
+import Foundation
+import UIKit
+
+struct DetailRecipeModel {
+  
+  let nameRecipe: String
+  let imageRecipe: UIImage
+  let instruction: [String]
+  let ingredients: [String]
+}
