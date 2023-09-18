ac401a43a27e199b23c1128cab2b8ebb061b324f
fullzoom <fullzoom@MacBook-Pro-fullzoom.local>
2023-09-18 23:56:24 +0200

    Fix Code

--- RecipesApp/Modules/Profile/ViewModel/ProfileViewModel.swift
+++ RecipesApp/Modules/Profile/ViewModel/ProfileViewModel.swift
@@ -1,6 +1,6 @@
-import Foundation
 import Firebase
 import FirebaseFirestore
+import Foundation
 
 class ProfileViewModel {
     private let db = Firestore.firestore()
@@ -8,39 +8,39 @@ class ProfileViewModel {
     var recipes: [CreateRecipeModel] = []
 
     func fetchRecipes(completion: @escaping (Result<[CreateRecipeModel], ProfileError>) -> Void) {
-            guard let userID = Auth.auth().currentUser?.uid else {
-                completion(.failure(.authenticationError))
+        guard let userID = Auth.auth().currentUser?.uid else {
+            completion(.failure(.authenticationError))
+            return
+        }
+
+        db.collection("Recipes").document(userID).collection("userRecipes").getDocuments { snapshot, error in
+            if let error = error {
+                completion(.failure(.firestoreError(error)))
                 return
             }
 
-            db.collection("Recipes").document(userID).collection("userRecipes").getDocuments { (snapshot, error) in
-                if let error = error {
-                    completion(.failure(.firestoreError(error)))
+            guard let snapshot = snapshot else {
+                completion(.failure(.unknownError))
+                return
+            }
+
+            var loadedRecipes: [CreateRecipeModel] = []
+            for document in snapshot.documents {
+                do {
+                    let recipe = try self.decodeRecipe(documentData: document.data())
+                    loadedRecipes.append(recipe)
+                } catch let error as ProfileError {
+                    completion(.failure(error))
                     return
-                }
-                
-                guard let snapshot = snapshot else {
+                } catch {
                     completion(.failure(.unknownError))
                     return
                 }
-
-                var loadedRecipes: [CreateRecipeModel] = []
-                for document in snapshot.documents {
-                    do {
-                        let recipe = try self.decodeRecipe(documentData: document.data())
-                        loadedRecipes.append(recipe)
-                    } catch let error as ProfileError {
-                        completion(.failure(error))
-                        return
-                    } catch {
-                        completion(.failure(.unknownError))
-                        return
-                    }
-                }
-                self.recipes = loadedRecipes
-                completion(.success(self.recipes))
             }
+            self.recipes = loadedRecipes
+            completion(.success(self.recipes))
         }
+    }
 
     private func decodeRecipe(documentData: [String: Any]) throws -> CreateRecipeModel {
         guard let jsonData = try? JSONSerialization.data(withJSONObject: documentData, options: []) else {
