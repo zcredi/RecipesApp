f9b5a8d16a250c16e99e5ffe2b85cc0d95aff786
fullzoom <fullzoom@MacBook-Pro-fullzoom.local>
2023-09-20 19:48:14 +0200

    Refactor code

--- RecipesApp/Modules/RecipeDetail/Model/DetailModel.swift
+++ RecipesApp/Modules/RecipeDetail/Model/DetailModel.swift
@@ -1,11 +1,1 @@
-import Foundation
-
-struct DetailRecipeModel {
-    var nameRecipe: String = ""
-    var imageRecipe: String = ""
-//    init?(name: String, image: String) {
-//        self.nameRecipe = name
-//        self.imageRecipe = image
-//    }
-}
 
