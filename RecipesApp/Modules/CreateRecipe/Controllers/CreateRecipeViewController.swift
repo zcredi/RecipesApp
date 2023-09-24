e62813bddfed85d25f21df08760089257ef893c0
fullzoom <fullzoom@MacBook-Pro-fullzoom.local>
2023-09-24 15:40:07 +0200

    Fix bug

--- RecipesApp/Modules/CreateRecipe/Controllers/CreateRecipeViewController.swift
+++ RecipesApp/Modules/CreateRecipe/Controllers/CreateRecipeViewController.swift
@@ -7,6 +7,12 @@ class CreateRecipeViewController: UIViewController {
     private lazy var cookTimeView = CookTimeView()
     private lazy var footerView = FooterView()
     
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        return scrollView
+    }()
+    
     private lazy var createRecipeButton: UIButton = {
         let button = UIButton(name: "Create recipe", backgroundColor: UIColor(named: "redBlue"), font: .poppinsBold16(), titleColor: .white)
         button.layer.cornerRadius = 8
@@ -60,16 +66,29 @@ class CreateRecipeViewController: UIViewController {
 
 extension CreateRecipeViewController {
     private func setupUI() {
-        view.addSubviews(headerView, cookTimeView, footerView, createRecipeButton)
-
+        view.addSubview(scrollView)
+        scrollView.addSubviews(headerView, cookTimeView, footerView, createRecipeButton)
+        
+        // Setup constraints for scrollView
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        if #available(iOS 15.0, *) {
+            NSLayoutConstraint.activate([
+                scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                scrollView.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor)
+            ])
+        }
+        // Setup constraints for headerView
         headerView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 4),
+            headerView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 4),
             headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             headerView.heightAnchor.constraint(equalToConstant: 370)
         ])
 
+        // Setup constraints for cookTimeView
         cookTimeView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             cookTimeView.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 5),
@@ -78,23 +97,30 @@ extension CreateRecipeViewController {
             cookTimeView.heightAnchor.constraint(equalToConstant: 60)
         ])
 
+        // Setup constraints for footerView
         footerView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            footerView.topAnchor.constraint(equalTo: cookTimeView.bottomAnchor, constant: 16),
-            footerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            footerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            footerView.heightAnchor.constraint(equalToConstant: 270),
-        ])
+        if #available(iOS 15.0, *) {
+            NSLayoutConstraint.activate([
+                footerView.topAnchor.constraint(equalTo: cookTimeView.bottomAnchor, constant: 16),
+                footerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+                footerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+                footerView.heightAnchor.constraint(equalToConstant: 200),
+                
+            ])
+        }
 
+        // Setup constraints for createRecipeButton
         createRecipeButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            createRecipeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
-            createRecipeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            createRecipeButton.heightAnchor.constraint(equalToConstant: 56),
-            createRecipeButton.widthAnchor.constraint(equalToConstant: 344)
+            createRecipeButton.topAnchor.constraint(equalTo: footerView.bottomAnchor, constant: 10),
+            createRecipeButton.widthAnchor.constraint(equalToConstant: 350),
+            createRecipeButton.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            createRecipeButton.heightAnchor.constraint(equalToConstant: 50),
+            createRecipeButton.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -10)
         ])
     }
 }
+
 
 extension CreateRecipeViewController: ServesButtonPressedDelegate {
     func servesButtonPressed(_ sender: UIButton) {
@@ -134,7 +160,6 @@ extension CreateRecipeViewController: SelectButtonTappedDelegate {
 }
 
 extension CreateRecipeViewController: RecipeImagePressedDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
     func recipeImagePressed(_ sender: UIImageView) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
